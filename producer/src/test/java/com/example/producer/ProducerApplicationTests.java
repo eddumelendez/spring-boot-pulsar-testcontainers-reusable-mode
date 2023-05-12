0d47b9153c4559859a43759b6fc774354e61cea8
Eddú Meléndez <eddu.melendez@gmail.com>
2023-05-11 19:05:34 -0600

    Add spring-boot-devtools

--- producer/src/test/java/com/example/producer/ProducerApplicationTests.java
+++ producer/src/test/java/com/example/producer/ProducerApplicationTests.java
@@ -1,13 +1,14 @@
 package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.PulsarContainer;
 import org.testcontainers.utility.DockerImageName;
 
-class ProducerApplicationTests {
+public class ProducerApplicationTests {
 
 	public static void main(String[] args) {
 		SpringApplication.from(ProducerApplication::main)
@@ -19,6 +20,7 @@ class ProducerApplicationTests {
 	static class ContainerConfiguration {
 
 		@Bean
+		@RestartScope
 		PulsarContainer pulsarContainer(DynamicPropertyRegistry registry) {
 			PulsarContainer pulsar = new PulsarContainer(DockerImageName.parse("apachepulsar/pulsar:2.11.0"))
 					.withReuse(true);
